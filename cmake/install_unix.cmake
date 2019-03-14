0b8f01bbd18cb16cafd389c6883c62158410fb46
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2019-03-14 13:35:03 +0200

    cmake/install_unix: Use root CMake source/binary dirs instead of current ones

--- cmake/install_unix.cmake
+++ cmake/install_unix.cmake
@@ -7,7 +7,7 @@ install(
     DESTINATION "${CMAKE_INSTALL_BINDIR}"
 )
 install(
-    FILES "${CMAKE_CURRENT_SOURCE_DIR}/data/${APP_FILE_NAME}.desktop"
+    FILES "${CMAKE_SOURCE_DIR}/data/${APP_FILE_NAME}.desktop"
     DESTINATION "${CMAKE_INSTALL_DATADIR}/applications"
 )
 install(
@@ -17,14 +17,11 @@ install(
 
 if (DPSO_COMPILE_PO)
     include(compile_po)
+    compile_po(mo_files "${CMAKE_BINARY_DIR}/locale")
 
-    compile_po(
-        mo_files
-        "${CMAKE_CURRENT_BINARY_DIR}/locale"
-    )
     install(
         # Note the trailing slash.
-        DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/locale/"
+        DIRECTORY "${CMAKE_BINARY_DIR}/locale/"
         DESTINATION "${CMAKE_INSTALL_LOCALEDIR}"
     )
 endif()
