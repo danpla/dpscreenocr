daf320d866d7ebaa20bdf582f1e1852e35b11bf4
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2019-03-16 12:39:32 +0200

    dpso_utils/ exec: Replace switch{} with if{}

--- src/dpso_utils/exec.cpp
+++ src/dpso_utils/exec.cpp
@@ -14,27 +14,21 @@ void dpsoExec(
     const char* exePath, const char* arg, int waitToComplete)
 {
     const auto pid = fork();
+    if (pid == -1)
+        return;
 
-    switch (pid) {
-        case -1:
-            return;
-            break;
-        case 0: {
-            const char* args[] = {exePath, arg, nullptr};
-            execvp(args[0], (char* const*)args);
-            // Use _Exit() instead of exit() since we don't want to
-            // call C++ destructors. In particular, this removes
-            // messages from Tesseract 4 ("~ObjectCache(): WARNING!
-            // LEAK!") and std::thread ("terminate called without an
-            // active exception").
-            std::_Exit(EXIT_FAILURE);
-            break;
-        }
-        default:
-            if (waitToComplete)
-                waitpid(pid, nullptr, 0);
-            break;
+    if (pid == 0) {
+        const char* args[] = {exePath, arg, nullptr};
+        execvp(args[0], (char* const*)args);
+        // Use _Exit() instead of exit() since we don't want to call
+        // C++ destructors. In particular, this removes messages from
+        // Tesseract (~ObjectCache(): WARNING! LEAK!) and std::thread
+        // (terminate called without an active exception).
+        std::_Exit(EXIT_FAILURE);
     }
+
+    if (waitToComplete)
+        waitpid(pid, nullptr, 0);
 }
 
 
