141174a969682b1305673a1766aa4baef7fb6e98
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2019-05-16 14:50:06 +0300

    ui/common: Use an empty string as fallback for APP_LOCALE_DIR

--- src/ui/common/paths.h
+++ src/ui/common/paths.h
@@ -2,4 +2,10 @@
 #pragma once
 
 
+/*
+ * Path to localization data to be passed to bindtextdomain(). Mainly
+ * intended for non-Apple Unix-like systems, where it's an absolute
+ * path pointing to installation prefix + "share/locale". On platforms
+ * that don't use it, it may be empty.
+ */
 extern const char* localeDir;
