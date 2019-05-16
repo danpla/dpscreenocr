a4061b0605fe47c3cfcdabaea85381799c6b595d
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2019-05-16 21:24:52 +0300

    dpso_utils: Implement fopenUtf8() on Windows

---
+++ src/dpso_utils/os_windows.cpp
@@ -0,0 +1,29 @@
+
+#include "os.h"
+
+#include <cerrno>
+
+#include "windows_utils.h"
+
+
+namespace dpso {
+
+
+std::FILE* fopenUtf8(const char* fileName, const char* mode)
+{
+    std::wstring fileNameUtf16;
+    std::wstring modeUtf16;
+
+    try {
+        fileNameUtf16 = win::utf8ToUtf16(fileName);
+        modeUtf16 = win::utf8ToUtf16(mode);
+    } catch (std::runtime_error&) {
+        errno = EINVAL;
+        return nullptr;
+    }
+
+    return _wfopen(fileNameUtf16.c_str(), modeUtf16.c_str());
+}
+
+
+}
