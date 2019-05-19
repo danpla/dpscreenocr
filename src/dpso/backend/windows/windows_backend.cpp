d922541126ff3c4271f91bded79d7a2bec974e5d
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2019-05-19 23:26:41 +0300

    dpso/backend/windows: Update comments

--- src/dpso/backend/windows/windows_backend.cpp
+++ src/dpso/backend/windows/windows_backend.cpp
@@ -6,20 +6,20 @@
 
 // We can't do anything in the main thread because its messages will
 // be handed by a GUI framework. Of course we can provide a low-level
-// routine to connect to a event filter provided by the framework, but
-// some frameworks may not provide such filters, and I'd like to keep
-// the API as high-level as possible.
+// routine to connect to an event filter provided by the framework,
+// but some frameworks may not provide such filters, and I'd like to
+// keep the API as high-level as possible.
 //
 // WindowsBackendExecutor and its proxy components do the job of
-// handling the implementation of Windows backend and its components
-// in the background thread. Please note that although absolutely
-// everything is called trough an executor, the only routines that
-// must be called in the background are the ones that use Windows API;
-// simple field getters don't technically need this.
+// handling the backend implementation in the background thread.
+// Please note that although absolutely everything is called trough an
+// executor, the only routines that must be called in the background
+// are the ones that use Windows API; others don't technically need
+// this.
 //
 // The solution is simple enough to provide the mentioned low-level
-// routine to handle a message from GUI framework, since it's possible
-// to choose between the background or in the main thread in runtime.
+// routine to handle messages from GUI framework, since it's possible
+// to choose between the background and the main thread in runtime.
 // For the latter, WindowsBackend::create() can simply instantiate
 // WindowsBackendImpl instead of WindowsBackendExecutor, or we can
 // tell WindowsBackendExecutor to use ActionExecutor that performs
