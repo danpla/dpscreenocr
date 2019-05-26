8a6f8ebd3728abe7667da3bf34b6a0deb5e097a8
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2019-05-26 19:52:20 +0300

    cmake/install_windows_msys2: Copy tessdata directory

--- cmake/install_windows_msys2.cmake
+++ cmake/install_windows_msys2.cmake
@@ -1,4 +1,25 @@
 
+# Instructions specific to MSYS2/MinGW.
+
+
+# Copy tessdata directory from share/ to dst_dir.
+function(copy_tessdata dst_dir)
+    set(SRC_DIR "$ENV{MINGW_PREFIX}/share/tessdata")
+
+    add_custom_command(
+        OUTPUT "${dst_dir}/tessdata"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "${SRC_DIR}" "${dst_dir}/tessdata"
+        DEPENDS "${SRC_DIR}"
+        VERBATIM
+    )
+
+    add_custom_target("tessdata_dir" ALL DEPENDS "${dst_dir}/tessdata")
+endfunction()
+
+
+copy_tessdata("${CMAKE_BINARY_DIR}")
+
+
 if (DPSO_UI STREQUAL "qt")
     include(qt_utils)
     include(get_linguas)
