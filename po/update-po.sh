eb05905c23b34de1deeb804f51d2b0116ba3b427
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2020-11-22 02:47:45 +0200

    po/update-po: Add --package-name

--- po/update-po.sh
+++ po/update-po.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 
-APPNAME='dpscreenocr'
+APP_NAME='dpScreenOCR'
+APP_FILE_NAME='dpscreenocr'
 BUGS_ADDRESS='https://github.com/danpla/dpscreenocr/issues'
 
-cp ../data/$APPNAME.desktop $APPNAME.desktop.in
+cp ../data/$APP_FILE_NAME.desktop $APP_FILE_NAME.desktop.in
 sed -e '/Comment\[/ d' \
     -e 's/Comment/_Comment/' \
-    -i $APPNAME.desktop.in
-intltool-extract --quiet --type=gettext/ini $APPNAME.desktop.in
+    -i $APP_FILE_NAME.desktop.in
+intltool-extract --quiet --type=gettext/ini $APP_FILE_NAME.desktop.in
 
 
 xgettext --files-from=POTFILES.in --from-code=UTF-8 --add-comments \
+    --package-name=$APP_NAME \
     --msgid-bugs-address=$BUGS_ADDRESS \
-    -D.. -D. --output=$APPNAME.pot \
+    -D.. -D. --output=$APP_FILE_NAME.pot \
     -k_ -kN_ -kpgettext_expr:1c,2 -knpgettext_expr:1c,2,3 \
-    $APPNAME.desktop.in.h
+    $APP_FILE_NAME.desktop.in.h
 
 for f in *.po;
 do
     [ -e "$f" ] || continue
     echo $f
-    msgmerge -q --update --no-fuzzy-matching --backup=off $f $APPNAME.pot;
+    msgmerge -q --update --no-fuzzy-matching --backup=off $f $APP_FILE_NAME.pot;
 done
 
 
 intltool-merge --quiet --desktop-style \
-    . $APPNAME.desktop.in ../data/$APPNAME.desktop
-rm -f $APPNAME.desktop.in.h
-rm -f $APPNAME.desktop.in
+    . $APP_FILE_NAME.desktop.in ../data/$APP_FILE_NAME.desktop
+rm -f $APP_FILE_NAME.desktop.in.h
+rm -f $APP_FILE_NAME.desktop.in
