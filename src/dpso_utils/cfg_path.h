30173f65ee7336a6e33ff7eba3059aa495635df7
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2021-11-22 03:56:32 +0200

    dpso_utils/cfg_path_unix_xdg: Return nullptr on errors

--- src/dpso_utils/cfg_path.h
+++ src/dpso_utils/cfg_path.h
@@ -20,6 +20,9 @@ extern "C" {
  * The function returns a platform-specific path where config files
  * can be saved. appName becomes a subdirectory of the path. The
  * directory chain is created if not already exists.
+ *
+ * On failure, and sets an error message (dpsoGetError()) and returns
+ * null.
  */
 const char* dpsoGetCfgPath(const char* appName);
 
--- src/dpso_utils/cfg_path_unix_xdg.cpp
+++ src/dpso_utils/cfg_path_unix_xdg.cpp
@@ -1,9 +1,12 @@
 
 #include "cfg_path.h"
 
+#include <cerrno>
 #include <cstdlib>
+#include <cstring>
 #include <string>
 
+#include "dpso/error.h"
 #include "os.h"
 #include "unix_utils.h"
 
@@ -15,21 +18,30 @@ const char* dpsoGetCfgPath(const char* appName)
     path.clear();
 
     const auto* xdgConfigHome = std::getenv("XDG_CONFIG_HOME");
+    // According to the specification, we should treat empty
+    // XDG_CONFIG_HOME as unset.
     if (xdgConfigHome && *xdgConfigHome)
         path += xdgConfigHome;
     else if (const auto* home = std::getenv("HOME")) {
         path += home;
         path += "/.config";
     }
+    else {
+        dpsoSetError(
+            "Neither XDG_CONFIG_HOME nor HOME environment variable "
+            "is set");
+        return nullptr;
+    }
 
-    // If even $HOME is not set, we will try to create appName
-    // dir in the current working directory.
-    if (!path.empty())
-        path += '/';
-
+    path += '/';
     path += appName;
 
-    dpso::unix::makeDirs(&path[0]);
+    if (!dpso::unix::makeDirs(&path[0])) {
+        dpsoSetError((
+            "makeDirs(\"" + path + "\") failed: "
+            + std::strerror(errno)).c_str());
+        return nullptr;
+    }
 
     return path.c_str();
 }
