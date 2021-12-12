d402fa374b89e3295d54a55c418678de78af7ef8
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2021-12-12 07:31:09 +0200

    dpso_utils/exec_posix: Don't make zombie processes

--- src/dpso_utils/exec_posix.cpp
+++ src/dpso_utils/exec_posix.cpp
@@ -7,23 +7,37 @@
 #include <unistd.h>
 
 
+static void exec(const char* exePath, const char* arg)
+{
+    const char* args[] = {exePath, arg, nullptr};
+    execvp(args[0], (char* const*)args);
+}
+
+
 void dpsoExec(
     const char* exePath, const char* arg, int waitToComplete)
 {
-    const auto pid = fork();
-    if (pid == -1)
+    const auto childPid = fork();
+    if (childPid == -1)
         return;
 
-    if (pid == 0) {
-        const char* args[] = {exePath, arg, nullptr};
-        execvp(args[0], (char* const*)args);
-        // Use _Exit() instead of exit() since we don't want to call
-        // C++ destructors. In particular, this removes messages from
-        // Tesseract (~ObjectCache(): WARNING! LEAK!) and std::thread
-        // (terminate called without an active exception).
+    if (childPid != 0) {
+        waitpid(childPid, nullptr, 0);
+        return;
+    }
+
+    if (waitToComplete) {
+        exec(exePath, arg);
         std::_Exit(EXIT_FAILURE);
     }
 
-    if (waitToComplete)
-        waitpid(pid, nullptr, 0);
+    const auto grandchildPid = fork();
+    if (grandchildPid == 0) {
+        exec(exePath, arg);
+        std::_Exit(EXIT_FAILURE);
+    }
+
+    // No waitpid(grandchildPid, ...) here: grandchildPid will be
+    // adopted by init.
+    std::_Exit(grandchildPid == -1 ? EXIT_FAILURE : EXIT_SUCCESS);
 }
