b13a189af8e5682ce0c1792f4f79a9dcf5a73bd5
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2022-04-05 22:26:31 +0300

    tests/test_cfg: Cleanup

--- tests/test_cfg.cpp
+++ tests/test_cfg.cpp
@@ -2,6 +2,7 @@
 #include <cerrno>
 #include <cstdio>
 #include <cstring>
+#include <initializer_list>
 #include <string>
 
 #include "dpso/dpso.h"
@@ -12,43 +13,73 @@
 #include "utils.h"
 
 
-static void testGet(
-    const DpsoCfg* cfg,
-    const char* key,
-    const char* defaultVal,
-    const char* expectedVal,
-    int lineNum)
-{
-    const auto* gotVal = dpsoCfgGetStr(cfg, key, defaultVal);
-    if (std::strcmp(gotVal, expectedVal) == 0)
-        return;
+namespace {
 
-    test::failure(
-        "line %i: dpsoGetStr(\"%s\", \"%s\"): "
-        "expected \"%s\", got \"%s\"\n",
-        lineNum,
-        key,
-        defaultVal,
-        test::utils::escapeStr(expectedVal).c_str(),
-        test::utils::escapeStr(gotVal).c_str());
+
+struct BasicTypesTest {
+    const char* key;
+
+    const char* strVal;
+    const char* strValDefault;
+
+    int intVal;
+    int intValDefault;
+
+    // We use int instead of bool since this is what cfg functions
+    // accept and return. It should be possible to check that an
+    // arbitrary int is set as "true" or "false" and, when used as
+    // defaultVal in dpsoCfgGetBool(), is normalized to 0 or 1.
+    int boolVal;
+    int boolValDefault;
+};
+
+
 }
 
 
-static void testGet(
+static std::string formatPossiblyNullStr(const char* str)
+{
+    if (!str)
+        return "nullptr";
+
+    return '"' + test::utils::escapeStr(str) + '"';
+}
+
+
+static void testGetStr(
     const DpsoCfg* cfg,
     const char* key,
-    int defaultVal,
+    const char* expectedVal,
+    const char* defaultVal)
+{
+    const auto* gotVal = dpsoCfgGetStr(cfg, key, defaultVal);
+    if ((!expectedVal && !gotVal)
+            || (expectedVal
+                && gotVal
+                && std::strcmp(gotVal, expectedVal) == 0))
+        return;
+
+    test::failure(
+        "dpsoGetStr(\"%s\", %s): expected %s, got %s\n",
+        key,
+        formatPossiblyNullStr(defaultVal).c_str(),
+        formatPossiblyNullStr(expectedVal).c_str(),
+        formatPossiblyNullStr(gotVal).c_str());
+}
+
+
+static void testGetInt(
+    const DpsoCfg* cfg,
+    const char* key,
     int expectedVal,
-    int lineNum)
+    int defaultVal)
 {
     const auto gotVal = dpsoCfgGetInt(cfg, key, defaultVal);
     if (gotVal == expectedVal)
         return;
 
     test::failure(
-        "line %i: dpsoGetInt(\"%s\", %i): "
-        "expected %i, got %i\n",
-        lineNum,
+        "dpsoGetInt(\"%s\", %i): expected %i, got %i\n",
         key,
         defaultVal,
         expectedVal,
@@ -56,25 +87,102 @@ static void testGet(
 }
 
 
-static void testGet(
+static void testGetBool(
     const DpsoCfg* cfg,
     const char* key,
-    bool defaultVal,
     bool expectedVal,
-    int lineNum)
+    int defaultVal)
 {
-    const bool gotVal = dpsoCfgGetBool(cfg, key, defaultVal);
+    // Note that gotVal is int so that we can check that defaultVal
+    // is normalized to 0 or 1.
+    const auto gotVal = dpsoCfgGetBool(cfg, key, defaultVal);
     if (gotVal == expectedVal)
         return;
 
     test::failure(
-        "line %i: dpsoGetBool(\"%s\", %s) "
-        "expected %s, got %s\n",
-        lineNum,
+        "dpsoGetBool(\"%s\", %i): expected %i, got %i\n",
         key,
-        test::utils::boolToStr(defaultVal).c_str(),
-        test::utils::boolToStr(expectedVal).c_str(),
-        test::utils::boolToStr(gotVal).c_str());
+        defaultVal,
+        expectedVal,
+        gotVal);
+}
+
+
+const char* const nonexistentKey = "nonexistent_key";
+
+
+const std::initializer_list<BasicTypesTest> strTests{
+    {"str_empty", "", "a", 5, 5, true, true},
+    {"str_leading_space", " a", "", 5, 5, true, true},
+    {"str_trailing_space", "a ", "", 5, 5, true, true},
+    {"str_single_space", " ", "", 5, 5, true, true},
+    {"str_control_chars", "\b\f\n\r\t", "", 5, 5, true, true},
+    {"str_escapes", R"(\b\f\n\r\t\\ \z)", "", 5, 5, true, true},
+    {"str_normal", "foo", "", 5, 5, true, true},
+    {"str_int_0", "0", "", 0, 1, false, true},
+    {"str_int_5", "5", "", 5, 0, true, false},
+    {"str_int_minus_5", "-5", "", -5, 0, true, false},
+    {"str_int_leading_space", " 5", "", 5, 0, true, false},
+    {"str_int_trailing_space", "5 ", "", 5, 0, true, false},
+    {"str_int_trailing_char", "5a", "", 0, 0, true, true},
+    {"str_int_trailing_space_and_char", "5 a", "", 0, 0, true, true},
+    {"str_float_period", "5.5", "", 0, 0, true, true},
+    {"str_float_comma", "5,5", "", 0, 0, true, true},
+    {"str_bool_true", "FaLsE", "", 0, 1, false, true},
+    {"str_bool_false", "TrUe", "", 1, 0, true, false},
+};
+
+
+const std::initializer_list<BasicTypesTest> intTests{
+    {"int_0", "0", "", 0, 5, false, true},
+    {"int_5", "5", "", 5, 0, true, false},
+    {"int_minus_5", "-5", "", -5, 0, true, false},
+};
+
+
+const std::initializer_list<BasicTypesTest> boolTests{
+    {"bool_true", "true", "", 1, 0, true, false},
+    {"bool_false", "false", "", 0, 1, false, true},
+    {"bool_true_set_as_10", "true", "", 1, 0, 10, false},
+    {"bool_true_set_as_minus_10", "true", "", 1, 0, -10, false},
+};
+
+
+static void setBasicTypes(DpsoCfg* cfg)
+{
+    for (const auto& test : strTests)
+        dpsoCfgSetStr(cfg, test.key, test.strVal);
+
+    for (const auto& test : intTests)
+        dpsoCfgSetInt(cfg, test.key, test.intVal);
+
+    for (const auto& test : boolTests)
+        dpsoCfgSetBool(cfg, test.key, test.boolVal);
+}
+
+
+static void getBasicTypes(const DpsoCfg* cfg)
+{
+    testGetStr(cfg, nonexistentKey, nullptr, nullptr);
+    testGetStr(cfg, nonexistentKey, "default", "default");
+
+    testGetInt(cfg, nonexistentKey, 5, 5);
+    testGetInt(cfg, nonexistentKey, -5, -5);
+
+    testGetBool(cfg, nonexistentKey, false, false);
+    testGetBool(cfg, nonexistentKey, true, true);
+    testGetBool(cfg, nonexistentKey, true, 5);
+    testGetBool(cfg, nonexistentKey, true, -5);
+
+    for (const auto& tests : {strTests, intTests, boolTests})
+        for (const auto& test : tests) {
+            testGetStr(
+                cfg, test.key, test.strVal, test.strValDefault);
+            testGetInt(
+                cfg, test.key, test.intVal, test.intValDefault);
+            testGetBool(
+                cfg, test.key, test.boolVal, test.boolValDefault);
+        }
 }
 
 
@@ -84,12 +192,11 @@ static std::string hotkeyToStr(const DpsoHotkey& hotkey)
 }
 
 
-static void testGet(
+static void testGetHotkey(
     const DpsoCfg* cfg,
     const char* key,
-    const DpsoHotkey& defaultVal,
     const DpsoHotkey& expectedVal,
-    int lineNum)
+    const DpsoHotkey& defaultVal)
 {
     DpsoHotkey gotVal;
     dpsoCfgGetHotkey(cfg, key, &gotVal, &defaultVal);
@@ -98,147 +205,12 @@ static void testGet(
         return;
 
     test::failure(
-        "line %i: dpsoCfgGetHotkey(\"%s\", &, {%s}) "
+        "dpsoCfgGetHotkey(\"%s\", &, {%s}): "
         "expected {%s}, got {%s}\n",
-        lineNum,
         key,
         hotkeyToStr(defaultVal).c_str(),
         hotkeyToStr(expectedVal).c_str(),
         hotkeyToStr(gotVal).c_str());
-}
-
-
-#define TEST_GET(cfg, key, defaultVal, expectedVal) \
-    testGet(cfg, key, defaultVal, expectedVal, __LINE__)
-
-
-static void getDefault(const DpsoCfg* cfg)
-{
-    const auto* key = "nonexistent_key";
-
-    TEST_GET(cfg, key, "", "");
-    TEST_GET(cfg, key, "default_value", "default_value");
-    TEST_GET(cfg, key, 0, 0);
-    TEST_GET(cfg, key, 1, 1);
-    TEST_GET(cfg, key, false, false);
-    TEST_GET(cfg, key, true, true);
-}
-
-
-static void testWhitespaceStr(DpsoCfg* cfg, bool set)
-{
-    static const char* const whitespaceStrings[] = {
-        " ",
-        " a",
-        "a ",
-        " \001 \b\f\n\r\t ",
-    };
-
-    static const auto numWhitespaceStrings = (
-        sizeof(whitespaceStrings) / sizeof(*whitespaceStrings));
-
-    for (std::size_t i = 0; i < numWhitespaceStrings; ++i) {
-        static char key[32];
-        std::snprintf(
-            key, sizeof(key),
-            "str_whitespace_%i", static_cast<int>(i));
-
-        const auto* str = whitespaceStrings[i];
-        if (set)
-            dpsoCfgSetStr(cfg, key, str);
-        else
-            TEST_GET(cfg, key, "", str);
-    }
-}
-
-
-const char* const backslashTestString = R"(\b\f\n\r\t\\ \z)";
-
-
-static void setStr(DpsoCfg* cfg)
-{
-    testWhitespaceStr(cfg, true);
-
-    dpsoCfgSetStr(cfg, "str_empty", "");
-    dpsoCfgSetStr(cfg, "str", "foo");
-
-    dpsoCfgSetStr(cfg, "str_int_123", "123");
-    dpsoCfgSetStr(cfg, "str_int_0", "0");
-    dpsoCfgSetStr(cfg, "str_float", "123.5");
-
-    dpsoCfgSetStr(cfg, "str_bool_1", "fAlSe");
-    dpsoCfgSetStr(cfg, "str_bool_2", "TrUe");
-
-    dpsoCfgSetStr(cfg, "str_backslash_test", backslashTestString);
-}
-
-
-static void getStr(DpsoCfg* cfg)
-{
-    testWhitespaceStr(cfg, false);
-
-    TEST_GET(cfg, "str_default", "default", "default");
-
-    TEST_GET(cfg, "str_empty", "default", "");
-
-    TEST_GET(cfg, "str", "", "foo");
-    TEST_GET(cfg, "str", 0, 0);
-    TEST_GET(cfg, "str", true, true);
-
-    TEST_GET(cfg, "str_int_123", "", "123");
-    TEST_GET(cfg, "str_int_123", 0, 123);
-    TEST_GET(cfg, "str_int_123", false, true);
-
-    TEST_GET(cfg, "str_int_0", "", "0");
-    TEST_GET(cfg, "str_int_0", 1, 0);
-    TEST_GET(cfg, "str_int_0", true, false);
-
-    // There are currently no routines to get/set floats, so don't
-    // treat them as integers, like strto*() do.
-    TEST_GET(cfg, "str_float", 0, 0);
-
-    TEST_GET(cfg, "str_bool_1", true, false);
-    TEST_GET(cfg, "str_bool_2", false, true);
-
-    TEST_GET(cfg, "str_backslash_test", "", backslashTestString);
-}
-
-
-static void setInt(DpsoCfg* cfg)
-{
-    dpsoCfgSetInt(cfg, "int_123", 123);
-    dpsoCfgSetInt(cfg, "int_0", 0);
-}
-
-
-static void getInt(const DpsoCfg* cfg)
-{
-    TEST_GET(cfg, "int_123", "", "123");
-    TEST_GET(cfg, "int_123", 0, 123);
-    TEST_GET(cfg, "int_123", false, true);
-
-    TEST_GET(cfg, "int_0", "", "0");
-    TEST_GET(cfg, "int_0", 1, 0);
-    TEST_GET(cfg, "int_0", true, false);
-}
-
-
-static void setBool(DpsoCfg* cfg)
-{
-    dpsoCfgSetBool(cfg, "bool_true", true);
-    dpsoCfgSetBool(cfg, "bool_false", false);
-}
-
-
-static void getBool(const DpsoCfg* cfg)
-{
-    TEST_GET(cfg, "bool_true", "", "true");
-    TEST_GET(cfg, "bool_true", 0, 1);
-    TEST_GET(cfg, "bool_true", false, true);
-
-    TEST_GET(cfg, "bool_false", "", "false");
-    TEST_GET(cfg, "bool_false", 1, 0);
-    TEST_GET(cfg, "bool_false", true, false);
 }
 
 
@@ -274,12 +246,12 @@ static void setHotkey(DpsoCfg* cfg)
 
 static void getHotkey(const DpsoCfg* cfg)
 {
-    const DpsoHotkey defaultHotkey {dpsoKeyF1, dpsoKeyModWin};
+    const DpsoHotkey defaultHotkey{dpsoKeyF1, dpsoKeyModWin};
 
-    TEST_GET(cfg, "hotkey_default", defaultHotkey, defaultHotkey);
+    testGetHotkey(cfg, nonexistentKey, defaultHotkey, defaultHotkey);
 
     for (const auto& test : hotkeyTests)
-        TEST_GET(cfg, test.key, defaultHotkey, test.hotkey);
+        testGetHotkey(cfg, test.key, test.hotkey, defaultHotkey);
 }
 
 
@@ -330,15 +302,20 @@ static void loadCfgData(DpsoCfg* cfg, const char* cfgData)
 
 static void testValueOverridingOnLoad(DpsoCfg* cfg)
 {
-    loadCfgData(cfg, "key value1\nkey value2\n");
-    TEST_GET(cfg, "key", "", "value2");
+    const std::string key = "duplicate_key";
+
+    loadCfgData(cfg, (key + " value1\n" + key + " value2").c_str());
+    testGetStr(cfg, key.c_str(), "value2", "");
 }
 
 
 static void testUnescapedBackslashAtEndOfQuotedString(DpsoCfg* cfg)
 {
-    loadCfgData(cfg, "key \"a\\\"");
-    TEST_GET(cfg, "key", "", "a");
+    const std::string key = (
+        "unescaped_backslash_at_end_of_quoted_string");
+
+    loadCfgData(cfg, (key + " \"a\\\"").c_str());
+    testGetStr(cfg, key.c_str(), "a", "");
 }
 
 
@@ -349,18 +326,12 @@ static void testCfg()
         test::fatalError(
             "dpsoCfgCreate() failed: %s\n", dpsoGetError());
 
-    setStr(cfg.get());
-    setInt(cfg.get());
-    setBool(cfg.get());
+    setBasicTypes(cfg.get());
     setHotkey(cfg.get());
 
     reload(cfg.get());
 
-    getDefault(cfg.get());
-
-    getStr(cfg.get());
-    getInt(cfg.get());
-    getBool(cfg.get());
+    getBasicTypes(cfg.get());
     getHotkey(cfg.get());
 
     testValueOverridingOnLoad(cfg.get());
