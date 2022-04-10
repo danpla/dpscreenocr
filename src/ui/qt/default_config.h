139d2ef6771225533cea5eae3a1a1cd82ae199dc
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2022-04-10 23:28:05 +0300

    ui/qt/default_config: Fix -Wexpansion-to-defined in newer GCC versions

--- src/ui/qt/default_config.h
+++ src/ui/qt/default_config.h
@@ -6,23 +6,16 @@
 #include <QtGlobal>
 
 
-#define NEED_SET_WINDOW_TITLE_WORKAROUND() \
-    (defined(Q_OS_UNIX) \
-        && !defined(Q_OS_DARWIN) \
-        && QT_VERSION >= QT_VERSION_CHECK(5, 0, 0) \
-        && QT_VERSION < QT_VERSION_CHECK(5, 3, 2))
-
-#ifndef DPSO_QT_X11_SET_WINDOW_TITLE_WORKAROUND
-    #if NEED_SET_WINDOW_TITLE_WORKAROUND()
+#if defined(Q_OS_UNIX)
+        && !defined(Q_OS_DARWIN)
+        && QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+        && QT_VERSION < QT_VERSION_CHECK(5, 3, 2)
+    #ifndef DPSO_QT_X11_SET_WINDOW_TITLE_WORKAROUND
         #define DPSO_QT_X11_SET_WINDOW_TITLE_WORKAROUND 1
-    #else
-        #define DPSO_QT_X11_SET_WINDOW_TITLE_WORKAROUND 0
     #endif
-#elif !NEED_SET_WINDOW_TITLE_WORKAROUND()
+#else
     #undef DPSO_QT_X11_SET_WINDOW_TITLE_WORKAROUND
 #endif
-
-#undef NEED_SET_WINDOW_TITLE_WORKAROUND
 
 
 #ifndef DPSO_QT_LOCAL_DATA
