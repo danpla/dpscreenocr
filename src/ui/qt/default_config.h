661ecc98ba5fdbc83bdd7de0ad89f0df782ed990
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2022-04-11 22:25:26 +0300

    ui/qt/default_config: Fix compilation on Unix

--- src/ui/qt/default_config.h
+++ src/ui/qt/default_config.h
@@ -6,9 +6,9 @@
 #include <QtGlobal>
 
 
-#if defined(Q_OS_UNIX)
-        && !defined(Q_OS_DARWIN)
-        && QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+#if defined(Q_OS_UNIX) \
+        && !defined(Q_OS_DARWIN) \
+        && QT_VERSION >= QT_VERSION_CHECK(5, 0, 0) \
         && QT_VERSION < QT_VERSION_CHECK(5, 3, 2)
     #ifndef DPSO_QT_X11_SET_WINDOW_TITLE_WORKAROUND
         #define DPSO_QT_X11_SET_WINDOW_TITLE_WORKAROUND 1
