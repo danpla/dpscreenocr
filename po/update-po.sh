da944e8932761b6b364ff42e1bc2d82db72cf66d
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2022-05-11 01:06:46 +0300

    po/update-po.sh: Add comment on why we don't use gettext for .desktop

--- po/update-po.sh
+++ po/update-po.sh
@@ -4,13 +4,16 @@ APP_NAME='dpScreenOCR'
 APP_FILE_NAME='dpscreenocr'
 BUGS_ADDRESS='https://github.com/danpla/dpscreenocr/issues'
 
+# gettext supports desktop entries since version 0.19. However, we
+# use intltool to make the script work on Ubuntu 14.04, which is
+# shipped with gettext 0.18.3.
 cp ../data/$APP_FILE_NAME.desktop $APP_FILE_NAME.desktop.in
 sed -e '/^Comment\[/ d' \
     -e 's/^Comment/_Comment/' \
     -i $APP_FILE_NAME.desktop.in
 intltool-extract --quiet --type=gettext/ini $APP_FILE_NAME.desktop.in
 
-
+# We include the current directory for the *.desktop.in.h file.
 xgettext --files-from=POTFILES.in --from-code=UTF-8 --add-comments \
     --package-name=$APP_NAME \
     --msgid-bugs-address=$BUGS_ADDRESS \
