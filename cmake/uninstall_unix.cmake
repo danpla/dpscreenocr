e419e8f27bd1c46637a90a9847bf28491c9e50a0
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2022-07-16 01:37:10 +0300

    cmake/uninstall_unix: Use CMAKE_CURRENT_LIST_DIR

--- cmake/uninstall_unix.cmake
+++ cmake/uninstall_unix.cmake
@@ -1,6 +1,6 @@
 if(NOT TARGET uninstall)
     configure_file(
-        "${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
+        "${CMAKE_CURRENT_LIST_DIR}/cmake_uninstall.cmake.in"
         "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake"
         @ONLY)
 
