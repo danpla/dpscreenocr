dff8444bec691b284eaf614f5e401dd3254ada7b
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2022-10-26 22:43:06 +0200

    ui/qt/about: Cleanup comment

--- src/ui/qt/about.cpp
+++ src/ui/qt/about.cpp
@@ -84,7 +84,7 @@ static QString joinInLayoutDirection(
     QString result;
 
     const auto rtl = QApplication::isRightToLeft();
-    // size_type since Qt 6 switched from int to qsizetype.
+    // Since Qt 6, size_type switched from int to qsizetype.
     for (QStringList::size_type i = 0; i < list.size(); ++i) {
         if (i > 0)
             result.insert(rtl ? 0 : result.size(), separator);
