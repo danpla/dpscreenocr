07cdb3d7bebf7430aab7eeba2fcd4ade45cca523
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2022-12-04 08:01:39 +0100

    po/update-po: Cleanup

--- po/update-po.sh
+++ po/update-po.sh
@@ -8,11 +8,11 @@ BUGS_ADDRESS='https://github.com/danpla/dpscreenocr/issues'
 
 xgettext --files-from=POTFILES.in \
     --from-code=UTF-8 \
-    --add-comments="Translators:" \
-    --package-name=$APP_NAME \
-    --msgid-bugs-address=$BUGS_ADDRESS \
+    --add-comments='Translators:' \
+    --package-name="$APP_NAME" \
+    --msgid-bugs-address="$BUGS_ADDRESS" \
     --directory=.. \
-    --output=$APP_FILE_NAME.pot \
+    --output="$APP_FILE_NAME.pot" \
     -k_ -kN_
 
 # Extract messages from the desktop entries as separate step instead
@@ -22,12 +22,12 @@ xgettext --from-code=UTF-8 \
     --omit-header \
     --join-existing \
     --directory=.. \
-    --output=$APP_FILE_NAME.pot \
+    --output="$APP_FILE_NAME.pot" \
     -k -kComment \
     data/dpscreenocr.desktop
 
 for f in *.po
 do
     msgmerge --quiet --update --no-fuzzy-matching --backup=off \
-        $f $APP_FILE_NAME.pot
+        "$f" "$APP_FILE_NAME.pot"
 done
