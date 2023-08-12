77f9080fc85eefd6381b656dd7203ecdda020d18
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2023-08-12 23:25:04 +0200

    Revert "ui/qt/about: Increase icon size"
    This change was made for Windows, but on Linux (where a SVG version
    is available) the icon is too big.

--- src/ui/qt/about.cpp
+++ src/ui/qt/about.cpp
@@ -89,7 +89,7 @@ About::About(QWidget* parent)
 
     auto* iconLabel = new QLabel();
     iconLabel->setPixmap(
-        getIcon(uiAppFileName).pixmap(fontHeight * 4.5f));
+        getIcon(uiAppFileName).pixmap(fontHeight * 4));
 
     auto* infoTextLabel = new QLabel();
     infoTextLabel->setTextFormat(Qt::RichText);
