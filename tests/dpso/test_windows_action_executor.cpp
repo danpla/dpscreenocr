e4c87011fdff45ff69af453fd42b5ba4e7f4cacf
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2023-12-02 16:58:46 +0100

    test/dpso: Add Windows backend executor exception tests for 3cdc50c3

---
+++ tests/dpso/test_windows_action_executor.cpp
@@ -0,0 +1,54 @@
+
+#include <stdexcept>
+
+#include "dpso/backend/windows/execution_layer/action_executor.h"
+
+#include "flow.h"
+#include "utils.h"
+
+
+namespace {
+
+
+void testExceptions()
+{
+    using namespace dpso::backend;
+
+    class TestException : public std::runtime_error {
+        using runtime_error::runtime_error;
+    };
+
+    auto actionExecutor = createBgThreadActionExecutor();
+
+    try {
+        execute(*actionExecutor, []{ throw TestException{""}; });
+        test::failure(
+            "testExceptions: Exception was not propagated by the "
+            "executor");
+        return;
+    } catch (TestException&) {
+    }
+
+    // There was a bug where an exception stored in std::exception_ptr
+    // under the hood was not cleared before being rethrown, and thus
+    // thrown for all further successful execute() calls.
+    try {
+        execute(*actionExecutor, []{});
+    } catch (TestException&) {
+        test::failure(
+            "testExceptions: The previously propagated exception was "
+            "rethrown on a successful call");
+    }
+}
+
+
+void testWindowsActionExecutor()
+{
+    testExceptions();
+}
+
+
+}
+
+
+REGISTER_TEST(testWindowsActionExecutor);
