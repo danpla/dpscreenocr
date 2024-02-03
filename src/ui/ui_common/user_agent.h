ef07ff0ea341d2ac72932e3b8dbb0219ef0bc70c
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2024-02-03 15:35:52 +0100

    ui/ui_common/user_agent: Add 'extern "C"'

--- src/ui/ui_common/user_agent.h
+++ src/ui/ui_common/user_agent.h
@@ -2,9 +2,19 @@
 #pragma once
 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 /**
  * Get a string suitable for the User-Agent HTTP header.
  *
  * The string is built from uiAppName and uiAppVersion.
  */
 const char* uiGetUserAgent(void);
+
+
+#ifdef __cplusplus
+}
+#endif
