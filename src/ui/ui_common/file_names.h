55fb14c69e366b6018829508a3b3ed0eb5901413
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2024-05-05 14:11:02 +0200

    ui/ui_common: Merge icon_names with file_names

--- src/ui/ui_common/file_names.h
+++ src/ui/ui_common/file_names.h
@@ -5,3 +5,10 @@
 extern const char* const uiAppFileName;
 extern const char* const uiCfgFileName;
 extern const char* const uiHistoryFileName;
+
+/* Main app icon. */
+extern const char* const uiIconNameApp;
+
+/* uiIconNameApp variants adjusted to display status. */
+extern const char* const uiIconNameAppBusy;
+extern const char* const uiIconNameAppError;
--- src/ui/ui_common/ui_common.h
+++ src/ui/ui_common/ui_common.h
@@ -6,7 +6,6 @@
 #include "cfg_default_values.h"
 #include "cfg_keys.h"
 #include "file_names.h"
-#include "icon_names.h"
 #include "init.h"
 #include "ocr_data_utils.h"
 #include "single_instance_guard.h"
