99179f3e38382319d2d5dada5ffcb9f72bee0ad8
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2024-11-27 20:21:44 +0100

    dpso_utils: Bring back version_cmp

---
+++ src/dpso_utils/version_cmp.cpp
@@ -0,0 +1,75 @@
+
+#include "version_cmp.h"
+
+#include <cassert>
+#include <charconv>
+#include <cstring>
+
+
+namespace dpso {
+
+
+VersionCmp::VersionCmp()
+    : VersionCmp{""}
+{
+}
+
+
+VersionCmp::VersionCmp(const char* versionStr)
+{
+    const auto* s = versionStr;
+    const auto* sEnd = s + std::strlen(s);
+
+    while (s < sEnd) {
+        // Use unsigned so that from_chars() doesn't accept the minus
+        // sign.
+        unsigned num;
+        const auto [end, ec] = std::from_chars(s, sEnd, num);
+        if (ec != std::errc{}) {
+            if (s > versionStr) {
+                // Make the preceding period a part of the extra
+                // string.
+                --s;
+                assert(!nums.empty());
+                assert(*s == '.');
+            }
+
+            break;
+        }
+
+        nums.push_back(num);
+
+        s = end;
+
+        if (*s == '.')
+            ++s;
+        else
+            break;
+    }
+
+    extra = s;
+}
+
+
+bool VersionCmp::operator<(const VersionCmp& other) const
+{
+    if (nums < other.nums)
+        return true;
+
+    if (nums > other.nums)
+        return false;
+
+    // 1.0-rc1 < 1.0
+    if (!extra.empty() && other.extra.empty())
+        return true;
+
+    // 1.0 > 1.0-rc1
+    if (extra.empty() && !other.extra.empty())
+        return false;
+
+    // 1.0-rc1 < 1.0-rc2
+    return extra < other.extra;
+}
+
+
+}
