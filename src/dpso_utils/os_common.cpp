80cbb38556f9c039e2a4823a6f605e5ba33eefc0
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2025-11-16 22:51:58 +0100

    dpso_utils/os_*: Use std::filesystem (Unix part)
    Since we already use std::filesystem in ui_common/init_user_data.cpp,
    it makes no sense to keep the old custom implementations of the OS
    functions.
    
    Currently, one test in test_os fails: GCC (but not Clang) says that
    path::parent_path() of "//a" is "/" instead of "//". This shouldn't be
    an issue in practice. We will see what to do with it once we implement
    the Windows parts of this task.

--- src/dpso_utils/os_common.cpp
+++ src/dpso_utils/os_common.cpp
@@ -1,14 +1,52 @@
 #include "os.h"
 
 #include <cstring>
+#include <filesystem>
 #include <optional>
+#include <system_error>
 
 #include "str.h"
 #include "stream/file_stream.h"
 #include "stream/utils.h"
 
 
+namespace fs = std::filesystem;
+
+
 namespace dpso::os {
+namespace {
+
+
+// When wrapping std::filesystem functions, we use error_code instead
+// of catching filesystem_error. The latter is merely a wrapper for
+// the former, so the only useful information of its what() is the
+// text from error_code::message(). Everything else (like paths passed
+// to the function, the function name itself, etc.) is unnecessary,
+// because the caller already knows this information. For example,
+// this is what() for rename() from GCC and Clang, respectively:
+//
+//   filesystem error: cannot rename: Permission denied [/dev] [/null]
+//   filesystem error: in rename: Permission denied ["/dev"] ["/null"]
+//
+// It also doesn't make sense to catch filesystem_error solely for its
+// code() when wrapping a single function: the error_code overloads
+// give fewer lines of code because there is no try-catch block.
+void check(const char* description, const std::error_code& ec)
+{
+    if (!ec)
+        return;
+
+    const auto message = str::format(
+        "{}: {}", description, ec.message());
+
+    if (ec == std::errc::no_such_file_or_directory)
+        throw FileNotFoundError{message};
+
+    throw Error{message};
+}
+
+
+}
 
 
 [[noreturn]]
@@ -23,6 +61,18 @@ void throwErrno(const char* description, int errnum)
     default:
         throw Error{message};
     }
+}
+
+
+std::string getDirName(const char* path)
+{
+    return fs::u8path(path).parent_path().u8string();
+}
+
+
+std::string getBaseName(const char* path)
+{
+    return fs::u8path(path).filename().u8string();
 }
 
 
@@ -45,6 +95,53 @@ const char* getFileExt(const char* filePath)
         return ext;
 
     return nullptr;
+}
+
+
+std::int64_t getFileSize(const char* filePath)
+{
+    std::error_code ec;
+    const auto result = fs::file_size(fs::u8path(filePath), ec);
+    check("fs::file_size", ec);
+    return result;
+}
+
+
+void resizeFile(const char* filePath, std::int64_t newSize)
+{
+    std::error_code ec;
+    fs::resize_file(fs::u8path(filePath), newSize, ec);
+    check("fs::resize_file", ec);
+}
+
+
+void removeFile(const char* filePath)
+{
+    std::error_code ec;
+    if (!fs::remove(fs::u8path(filePath), ec))
+        // If the entry does not exist, std::filesystem::remove()
+        // returns false instead of reporting an error. Emulate this
+        // case manually.
+        throw FileNotFoundError{
+            "No such file or directory (false from fs::remove())"};
+
+    check("fs::remove", ec);
+}
+
+
+void replace(const char* src, const char* dst)
+{
+    std::error_code ec;
+    fs::rename(fs::u8path(src), fs::u8path(dst), ec);
+    check("fs::rename", ec);
+}
+
+
+void makeDirs(const char* dirPath)
+{
+    std::error_code ec;
+    fs::create_directories(fs::u8path(dirPath), ec);
+    check("fs::create_directories", ec);
 }
 
 
--- src/dpso_utils/os_unix.cpp
+++ src/dpso_utils/os_unix.cpp
@@ -4,8 +4,6 @@
 #include <fcntl.h>
 #include <locale.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -22,33 +20,6 @@ namespace {
 void throwErrno(const char* description)
 {
     os::throwErrno(description, errno);
-}
-
-
-struct PathParts {
-    const char* dirBegin;
-    const char* dirEnd;
-    const char* baseBegin;
-    const char* baseEnd;
-};
-
-
-PathParts splitPath(const char* path)
-{
-    const auto* sepBegin = path;
-    const auto* sepEnd = sepBegin;
-
-    const auto* s = path;
-
-    for (; *s; ++s)
-        if (*s == '/') {
-            if (s > path && s[-1] != '/')
-                sepBegin = s;
-
-            sepEnd = s + 1;
-        }
-
-    return {path, sepBegin == path ? sepEnd : sepBegin, sepEnd, s};
 }
 
 
@@ -88,97 +59,15 @@ const char* const newline = "\n";
 const char* const dirSeparators = "/";
 
 
-std::string getDirName(const char* path)
-{
-    const auto parts = splitPath(path);
-    return {parts.dirBegin, parts.dirEnd};
-}
-
-
-std::string getBaseName(const char* path)
-{
-    const auto parts = splitPath(path);
-    return {parts.baseBegin, parts.baseEnd};
-}
-
-
 std::string convertUtf8PathToSys(const char* utf8Path)
 {
     return utf8Path;
 }
 
 
-std::int64_t getFileSize(const char* filePath)
-{
-    struct stat st;
-    if (stat(filePath, &st) != 0)
-        throwErrno("stat()");
-
-    return st.st_size;
-}
-
-
-void resizeFile(const char* filePath, std::int64_t newSize)
-{
-    if (truncate(filePath, newSize) != 0)
-        throwErrno("truncate()");
-}
-
-
 std::FILE* fopen(const char* filePath, const char* mode)
 {
     return ::fopen(filePath, mode);
-}
-
-
-void removeFile(const char* filePath)
-{
-    if (unlink(filePath) != 0)
-        throwErrno("unlink()");
-}
-
-
-void replace(const char* src, const char* dst)
-{
-    if (rename(src, dst) != 0)
-        throwErrno("rename()");
-}
-
-
-static void makeDirs(char* dirPath, mode_t mode)
-{
-    auto* s = dirPath;
-
-    // Root always exists.
-    while (*s == '/')
-        ++s;
-
-    while (*s) {
-        while (*s && *s != '/')
-            ++s;
-
-        while (*s == '/')
-            ++s;
-
-        const auto c = *s;
-        *s = 0;
-        // Force 0777 mode for intermediate directories so that mkdir
-        // can create a directory with read or write permissions
-        // removed when the same permissions are used for a newly
-        // created parent directory.
-        const auto ret = mkdir(dirPath, c ? 0777 : mode);
-        *s = c;
-
-        if (ret != 0 && errno != EEXIST)
-            throwErrno("mkdir()");
-    }
-}
-
-
-void makeDirs(const char* dirPath)
-{
-    std::string dirPathCopy{dirPath};
-    makeDirs(dirPathCopy.data(), 0777);
 }
 
 
