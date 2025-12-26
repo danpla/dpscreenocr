15043fe4e5fbacd4e23ad8ce3c386ebc0ccbbbf0
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2025-12-26 18:40:29 +0100

    dpso_utils/stream: Remove StringStream
    It's only used to test OutNewlineConversionStream.

--- tests/dpso_utils/stream/test_out_newline_conversion_stream.cpp
+++ tests/dpso_utils/stream/test_out_newline_conversion_stream.cpp
@@ -1,9 +1,11 @@
+#include <optional>
+
 #include "flow.h"
 #include "utils.h"
 
 #include "dpso_utils/os.h"
+#include "dpso_utils/stream/file_stream.h"
 #include "dpso_utils/stream/out_newline_conversion_stream.h"
-#include "dpso_utils/stream/string_stream.h"
 #include "dpso_utils/stream/utils.h"
 
 
@@ -12,6 +14,7 @@ namespace {
 
 void testOutNewlineConversionStream()
 {
+    const auto* fileName = "test_out_newline_conversion_stream.txt";
     const auto* input = " \n \r \r\n ";
 
     const struct {
@@ -25,21 +28,35 @@ void testOutNewlineConversionStream()
     };
 
     for (const auto& test : tests) {
-        dpso::StringStream strStream;
+        std::optional<dpso::FileStream> file;
+        try {
+            file.emplace(fileName, dpso::FileStream::Mode::write);
+        } catch (dpso::os::Error& e) {
+            test::fatalError(
+                "FileStream(\"{}\", Mode::write): {}",
+                fileName, e.what());
+        }
+
         dpso::OutNewlineConversionStream convStream{
-            strStream, test.outNewline};
+            *file, test.outNewline};
 
         dpso::write(convStream, input);
 
-        if (strStream.getStr() != test.expected)
+        file.reset();
+        const auto actual = test::utils::loadText(
+            "testOutNewlineConversionStream", fileName);
+
+        if (actual != test.expected)
             test::failure(
                 "Converting {} to {} newlines: expected {}, got {}",
                 test::utils::toStr(input),
                 test.outNewline
                     ? test::utils::toStr(test.outNewline) : "OS",
                 test::utils::toStr(test.expected),
-                test::utils::toStr(strStream.getStr()));
+                test::utils::toStr(actual));
     }
+
+    test::utils::removeFile(fileName);
 }
 
 
