c162f8248de5d4798580edf6b23dc71c3d10f8ba
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2026-03-21 21:07:52 +0100

    dpso_utils/os: Remove unnecessary <memory>

--- src/dpso_utils/os.h
+++ src/dpso_utils/os.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstdint>
-#include <memory>
 #include <stdexcept>
 #include <string>
 #include <string_view>
--- src/dpso_utils/os_stdio.h
+++ src/dpso_utils/os_stdio.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdio>
+#include <memory>
 
 
 namespace dpso::os {
