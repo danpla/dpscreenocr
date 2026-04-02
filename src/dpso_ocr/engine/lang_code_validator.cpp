bd910e3ffb4ed27aef61487a3d7e4429c27aea87
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2026-04-02 18:07:07 +0200

    dpso_ocr/engine/lang_code_validator: Include invalid char in exception message

--- src/dpso_ocr/engine/lang_code_validator.cpp
+++ src/dpso_ocr/engine/lang_code_validator.cpp
@@ -1,6 +1,6 @@
 #include "engine/lang_code_validator.h"
 
-#include <cstddef>
+#include <algorithm>
 
 #include "dpso_utils/str.h"
 
@@ -8,7 +8,7 @@
 namespace dpso::ocr {
 
 
-static bool isValidLangCodeChar(char c)
+static bool isValidChar(char c)
 {
     return
         (c >= '0' && c <= '9')
@@ -25,10 +25,12 @@ void validateLangCode(std::string_view langCode)
     if (langCode.empty())
         throw LangCodeError{"Language code is empty"};
 
-    for (std::size_t i{}; i < langCode.size(); ++i)
-        if (!isValidLangCodeChar(langCode[i]))
-            throw LangCodeError{str::format(
-                "Invalid character at index {}", i)};
+    if (const auto iter = std::find_if_not(
+                langCode.begin(), langCode.end(), isValidChar);
+            iter != langCode.end())
+        throw LangCodeError{str::format(
+            "Invalid character '{}' at index {}",
+            *iter, iter - langCode.begin())};
 }
 
 
